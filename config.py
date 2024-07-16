c7f58f755166cc74fc87b945b9449f91ce32b6d4
dzstreax <masternoob1789@gmail.com>
2024-07-16 18:28:28 +0700

    masive

--- config.py
+++ config.py
@@ -1,87 +1,90 @@
-import os
+# (©)Codexbotz
+# Recife By Zaen @Mafia_Tobatz
+# Kalo clone Gak usah hapus 
+# gue tandain akun tele nya ngentod
+
+
 import logging
+import os
 from logging.handlers import RotatingFileHandler
 
-#Bot token @Botfather
+# Bot token dari @Botfather
 TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "7318300249:AAEN_DZSbdikQXmBpzyMaUwzQSyoPWYp8rQ")
 
-#Your API ID & API HASH from my.telegram.org [https://youtu.be/gZQJ-yTMkEo?si=H4NlUUgjsIc5btzH]
-#Your API ID from my.telegram.org
+# API ID Anda dari my.telegram.org
 APP_ID = int(os.environ.get("APP_ID", "28109271"))
 
-#Your API Hash from my.telegram.org
+# API Hash Anda dari my.telegram.org
 API_HASH = os.environ.get("API_HASH", "f87196066130b05e25829c77c64b5b90")
 
-#Your db channel Id
+# ID Channel Database
 CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002239210703"))
 
-#OWNER ID
+# OWNER ID
 OWNER_ID = int(os.environ.get("OWNER_ID", "6577039739"))
 
-#Port
-PORT = os.environ.get("PORT", "8080")
+# NAMA OWNER
+OWNER = os.environ.get("OWNER", "@Arkan1789")
 
-#Database 
-#Database [https://youtu.be/qFB0cFqiyOM?si=fVicsCcRSmpuja1A]
+# Database
 DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://masternoob1789:VH7wRi7XXXOBBdBt@cluster0.twoj8ai.mongodb.net/")
 DB_NAME = os.environ.get("DATABASE_NAME", "Cluster0")
 
-#Shortner (token system) 
-# check my discription to help by using my refer link of shareus.io
-# 
+# Username CH & Group
+CHANNEL = os.environ.get("CHANNEL", "dinomerah_desahh")
+GROUP = os.environ.get("GROUP", "dinomerahdesah")
 
-SHORTLINK_URL = os.environ.get("SHORTLINK_URL", "publicearn.com")
-SHORTLINK_API = os.environ.get("SHORTLINK_API", "b323441bc55ef14be63018992134d393a62a9da9")
-VERIFY_EXPIRE = int(os.environ.get('VERIFY_EXPIRE', 86400)) # Add time in seconds
-IS_VERIFY = os.environ.get("IS_VERIFY", "True")
-TUT_VID = os.environ.get("TUT_VID", "https://t.me/Ultroid_Official/18") # shareus ka tut_vid he 
+# ID dari Channel Atau Group Untuk Wajib Subscribenya
+FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "dinomerah_desahh"))
+FORCE_SUB_GROUP = int(os.environ.get("FORCE_SUB_GROUP", "dinomerahdesah"))
 
-#force sub channel id, if you want enable force sub
-FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1002152821639"))
+# ID dari Channel Atau Group Untuk Wajib Subscribenya
+FORCE_SUB_CHANNEL1 = int(os.environ.get("FORCE_SUB_CHANNEL1", "0"))
+FORCE_SUB_GROUP1 = int(os.environ.get("FORCE_SUB_GROUP1", "0"))
+
+# ID dari Channel Atau Group Untuk Wajib Subscribenya
+FORCE_SUB_CHANNEL2 = int(os.environ.get("FORCE_SUB_CHANNEL2", "0"))
+
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
-#start messages
-START_MSG = os.environ.get("START_MESSAGE", "<b>Hello {first} Anda harus bergabung di Channel/Grup saya untuk menggunakan saya. Silakan Join Ke Channel Terlebih Dahulu</b>")
+# Pesan Awalan /start
+START_MSG = os.environ.get(
+    "START_MESSAGE",
+    "<b>Hello {first}</b>\n\n<b>Saya dapat menyimpan file pribadi di Channel Tertentu dan pengguna lain dapat mengaksesnya dari link khusus.</b>",
+)
 try:
-    ADMINS=[]
-    for x in (os.environ.get("ADMINS", "7318300249").split()):
-        ADMINS.append(int(x))
+    ADMINS = [int(x) for x in (os.environ.get("ADMINS", "7318300249").split())]
 except ValueError:
-        raise Exception("Your Admins list does not contain valid integers.")
+    raise Exception("Daftar Admin Anda tidak berisi User ID Telegram yang valid.")
 
-#Force sub message 
-FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "<b>Hello {first}Saya dapat menyimpan file pribadi di Channel Tertentu dan pengguna lain dapat mengaksesnya dari link khusus.</b>")
+# Pesan Saat Memaksa Subscribe
+FORCE_MSG = os.environ.get(
+    "FORCE_SUB_MESSAGE",
+    "<b>Hello {first}\n\nAnda harus bergabung di Channel/Grup saya Terlebih dahulu untuk Melihat File yang saya Bagikan\n\nSilakan Join Ke Channel & Group Terlebih Dahulu</b>",
+)
 
-#set your Custom Caption here, Keep None for Disable Custom Caption
-CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", "This video/Photo/anything is available on the internet. We LeakHubd or its subsidiary channel doesn't produce any of them.")
+# Atur Teks Kustom Anda di sini, Simpan (None) untuk Menonaktifkan Teks Kustom
+CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", None)
 
-#set True if you want to prevent users from forwarding files from bot
-PROTECT_CONTENT = True if os.environ.get('PROTECT_CONTENT', "False") == "True" else False
-
-#Set true if you want Disable your Channel Posts Share button
-DISABLE_CHANNEL_BUTTON = os.environ.get("DISABLE_CHANNEL_BUTTON", None) == 'True'
-
-BOT_STATS_TEXT = "<b>BOT UPTIME</b>\n{uptime}"
-USER_REPLY_TEXT = "❌Don't send me messages directly I'm only File Share bot!"
+# Setel True jika Anda ingin Menonaktifkan tombol Bagikan Kiriman Saluran Anda
+DISABLE_CHANNEL_BUTTON = os.environ.get("DISABLE_CHANNEL_BUTTON", None) == "True"
 
 ADMINS.append(OWNER_ID)
-ADMINS.append(6695586027)
+ADMINS.append(1250450587)
+ADMINS.append(844432220)
+ADMINS.append(1750080384)
 
 LOG_FILE_NAME = "filesharingbot.txt"
 
 logging.basicConfig(
     level=logging.INFO,
-    format="[%(asctime)s - %(levelname)s] - %(name)s - %(message)s",
-    datefmt='%d-%b-%y %H:%M:%S',
+    format="[%(levelname)s] - %(name)s - %(message)s",
+    datefmt="%d-%b-%y %H:%M:%S",
     handlers=[
-        RotatingFileHandler(
-            LOG_FILE_NAME,
-            maxBytes=50000000,
-            backupCount=10
-        ),
-        logging.StreamHandler()
-    ]
+        RotatingFileHandler(LOG_FILE_NAME, maxBytes=50000000, backupCount=10),
+        logging.StreamHandler(),
+    ],
 )
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
